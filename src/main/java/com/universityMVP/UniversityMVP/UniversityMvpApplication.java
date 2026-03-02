a1fb2cbc97758b4753317f61249a4dea221a4f5a
Tchezery Ribeiro <tchezery@Tchezerys-MacBook-Air.local>
2026-03-02 19:50:09 -0300

    up SpringBoot structure

---
+++ src/main/java/com/universityMVP/UniversityMVP/UniversityMvpApplication.java
@@ -0,0 +1,13 @@
+package com.universityMVP.UniversityMVP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversityMvpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniversityMvpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/universityMVP/UniversityMVP/UniversityMvpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.universityMVP.UniversityMVP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversityMvpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
