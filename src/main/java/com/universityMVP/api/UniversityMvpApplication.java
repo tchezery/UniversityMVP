2997d4b1af8efd5ff3c0904daf531ff0e40a70c5
tchezery <tchezeryribeiro@gmail.com>
2026-03-02 20:11:19 -0300

    update path

--- src/main/java/com/universityMVP/api/UniversityMvpApplication.java
+++ src/main/java/com/universityMVP/api/UniversityMvpApplication.java
@@ -1,4 +1,4 @@
-package com.universityMVP.UniversityMVP;
+package com.universityMVP.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
