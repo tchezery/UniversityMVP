a7146446da3a2376beb730977cc131c083c5c21e
tchezery <tchezeryribeiro@gmail.com>
2026-03-03 00:38:52 -0300

    info role

--- src/main/java/com/universityMVP/api/model/Role.java
+++ src/main/java/com/universityMVP/api/model/Role.java
@@ -1,5 +1,7 @@
 package com.universityMVP.api.model;
 
+// Enum to represent user roles in the system
+// Not is a table in this case, is just restriction name to the user role
 public enum Role 
 {
     STUDENT,
