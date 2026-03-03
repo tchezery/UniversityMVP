7d9d8914113c198b3c0e5c5ff0a37fc78cee97bd
tchezery <tchezeryribeiro@gmail.com>
2026-03-03 00:06:27 -0300

    add user roles

---
+++ src/main/java/com/universityMVP/api/model/Role.java
@@ -0,0 +1,8 @@
+package com.universityMVP.api.model;
+
+public enum Role 
+{
+    STUDENT,
+    TEACHER,
+    ADMIN
+}
--- src/main/java/com/universityMVP/api/model/User.java
+++ src/main/java/com/universityMVP/api/model/User.java
@@ -1,10 +1,14 @@
 package com.universityMVP.api.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Column;
+import com.universityMVP.api.model.Role;
 
 //model == blueprint
 
@@ -22,10 +26,24 @@ public class User
     //Key above @Id
     private Long id;
     private String name;
+    private String email;
+    private String password;
+    
+    // Define the role attribute as an enumerated type and store it as a string in the database
+    @Enumerated(EnumType.STRING)
+    // Define the role attribute as not nullable in the database
+    @Column(nullable = false)
+    private Role role;
 
     // Getters and Setters of attributes
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
     public String getName() { return name; }
     public void setName(String name) { this.name = name; }
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+    public Role getRole() { return role; }
+    public void setRole(Role role) { this.role = role; }
 }
